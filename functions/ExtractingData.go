e3c7b5e0e0ee737b1e7bdc8879d6cd666bdd447d
AymenOski <aymenazizi610@gmail.com>
2025-03-11 12:07:09 +0000

    Improving parsing

--- functions/ExtractingData.go
+++ functions/ExtractingData.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 )
 
-func ExtractingDate() (int, string, string, map[string][]string) {
+func ExtractingDate() (int, string, string, map[string][]string, []string) {
 	file, err := os.Open(os.Args[1])
 	if err != nil {
 		fmt.Println(err)
@@ -21,14 +21,18 @@ func ExtractingDate() (int, string, string, map[string][]string) {
 	antNum := 0
 	startingRoom := ""
 	endingRoom := ""
-	// FC denoted as "format checker"
 	i := 0
+	// FC denoted as "format checker"
 	FC_StartFlag := 0
 	FC_EndFlag := 0
 	FC_AntNumber := -1
 	Tunnels := make(map[string][]string)
+	Rooms := make([]string, 0)
 	check := false
 	for scanner.Scan() {
+		// if strings.HasPrefix(scanner.Text(), "#") || strings.HasPrefix(scanner.Text(), "L") {
+		// 	continue
+		// }
 		if c == 0 {
 			antNum, err = strconv.Atoi(scanner.Text())
 			if antNum <= 0 {
@@ -53,10 +57,10 @@ func ExtractingDate() (int, string, string, map[string][]string) {
 		// storing data after the ##start flag
 		if check {
 			if len(scanner.Text()) > 0 {
-				startingRoom = scanner.Text()[:1]
-				check = false
+				Rooms = append(Rooms, scanner.Text()[:1])
 			} else {
 				fmt.Println("ERROR: invalid data format, Missing ants starting room")
+				os.Exit(0)
 			}
 		} else if !check && c == 2 {
 			if len(scanner.Text()) > 0 {
@@ -64,6 +68,7 @@ func ExtractingDate() (int, string, string, map[string][]string) {
 				c = 3
 			} else {
 				fmt.Println("ERROR: invalid data format, Missing ants ending room")
+				os.Exit(0)
 			}
 		}
 		if scanner.Text() == "##start" {
@@ -71,10 +76,13 @@ func ExtractingDate() (int, string, string, map[string][]string) {
 			check = true
 		} else if scanner.Text() == "##end" {
 			FC_EndFlag = i
+			check = false
 			c = 2
 		}
 		i++
 	}
+	// startingRoom = Rooms[0]
+	Rooms = append(Rooms, endingRoom)
 	FormatChekcer(FC_AntNumber, FC_StartFlag, FC_EndFlag, i)
-	return antNum, startingRoom, endingRoom, Tunnels
+	return antNum, startingRoom, endingRoom, Tunnels, Rooms
 }
